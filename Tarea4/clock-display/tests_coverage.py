e4ef34de1f6e4cf88d17051bf8e0d5c971ef820c
Dafne Arriagada <dafne.arriagada@uc.cl>
2023-04-18 20:37:23 -0400

    T4: ACT1

---
+++ Tarea4/clock-display/tests_coverage.py
@@ -0,0 +1,15 @@
+import unittest
+#importar módulos a testear.
+
+''' Tests que buscan 100% coverage.
+'''
+
+class TestSrc(unittest.TestCase):
+
+    pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
