1bab758d10558791c691707b381fb3621cc9e362
Dafne Arriagada <dafne.arriagada@uc.cl>
2023-04-27 20:55:47 -0400

    T4: COVERAVE ACT 1 UPDATED

--- Tarea4/clock-display/tests/tests_coverage.py
+++ Tarea4/clock-display/tests/tests_coverage.py
@@ -1,113 +1,153 @@
-# import unittest
-# #importar módulos a testear.
-# from src.clock_factory import *
-# from src.display_number import *
+import unittest
+#importar módulos a testear.
+from src.clock_factory import *
+from src.display_number import *
 
-# ''' Tests que buscan 100% coverage.
-# '''
+''' Tests que buscan 100% coverage.
+'''
 
-# class TestSrc(unittest.TestCase):
+class TestSrc(unittest.TestCase):
 
-#     def test1(self):
-#         clock = ClockDisplay([23,60])
-#         for i in range(20):
-#             clock.increment()
-#             # print(clock.str())
+    ## TEST UNITARIOS: INSTANCIAS TODAS LAS CLASES DEL PROYECTO
 
-#     def test2(self):
-#         clock = ClockDisplay([23,60])
-#         for i in range(20):
-#             clock.increment()
-#         clock.clone()
-#         # print(clock.str())
-    
-#     def test3(self):
-#         clock = ClockDisplay([23, 60])
-#         for i in range(50):
-#             clock.increment()
-#         clock.invariant()
-#         # print(clock.str())
+    ## TESTS NUMBER DISPLAY
+    def test1(self):
+        display = NumberDisplay(10, 20)
+        display.increase() ## value = 11 -> False
+        result = display.str()
+        self.assertEqual(result, "11")
 
-#     def test4(self):    # testea linea 13 clockdisplay
-#         clock = ClockDisplay([23, 60])
-#         for i in range(250):
-#             clock.increment()
+    def test2(self):
+        display = NumberDisplay(10, 20)
+        display.reset() ## 00
+        result = display.str() ## explora todos los casos del str()
+        self.assertEqual(result, "00")
 
-#     def test5(self):
-#         clock = ClockDisplay([23, 60])
-#         clock.increment()
-#         # resetear clock:
-#         clock_reseteado = list(map(lambda n: n.reset(), clock.numbers))
+    def test3(self):
+        display = NumberDisplay(9, 20)
+        result = display.invariant()
+        self.assertEqual(result, True)
 
-#     def test6(self):    # testea línea 12 en clockdisplay
-#         clock = ClockDisplay([24,60])
-#         clock.numbers = []
-#         clock.increment()
+    def test4(self):
+        # que clone correctamente
+        display = NumberDisplay(10, 20)
+        display_clonado = display.clone()
+        result = display_clonado.str()
+        self.assertEqual(result, "10")
 
-#     def test7(self):
-#         clock = ClockDisplay([24, 60, 60])
-#         for i in range(4):
-#             clock.increment()
-#         assert clock.str() == "00:00:04"
+        
+    # ## TESTS CLOCK DISPLAY
 
-#     def test8(self): # NO FUNCIONANDO
-#         print("TEST 8")
-#         clock = ClockDisplay([24, 60])
-#         print(clock.str())
-#         print(clock.numbers[0].str())
-#         print(clock.numbers[1].str())
-#         clock.numbers.pop()
-#         print(clock.str())
-#         print("largo", len(clock.numbers) - 1)
-#         clock.increment()
+    def test5(self):
+        # que incremente corectamente cuando se alcanza limite
+        # del secundero.
 
-#     def test9(self):
-#         clock = ClockDisplay([24, 60])
-#         clock.numbers[1].increase()
-#         assert clock.numbers[1].str() == "01"
+        ## entregamos iterable con límite valor de cada rango
+        ## hh:mm:ss:mmmm
+        ## hh:mm:ss
+        ## hh:mm
 
-#     def test10(self):
-#         clock = ClockDisplay([24, 60])
-#         clock.numbers[1].value = 100
-#         assert clock.numbers[1].str() == "100"
+        clock_display = ClockDisplay([24,60,60])
+        clock_display.numbers = [NumberDisplay(0,24), 
+                                NumberDisplay(0,60),
+                                NumberDisplay(59,60)]
+        clock_display.increment()
+        result = clock_display.str()
+        self.assertEqual(result, "00:01:00")
+        
 
-#     def test11(self):   # display_number 21: mutation from <class 'ast.Lt'> to <class 'ast.LtE'>
-#         clock = ClockDisplay([24, 60])
-#         clock.numbers[1].value = 60
-#         clock.numbers[1].limit = 60
-#         assert clock.numbers[1].invariant() is False
+    def test6(self):
+        clock_display = ClockDisplay([24,60,60])
+        clock_clonado = clock_display.clone()
+        result = clock_clonado.str()
+        self.assertEqual(result, "00:00:00")
 
-#     def test12(self):    # display_number 21: mutation from <class 'ast.Lt'> to <class 'ast.Gt'>
-#         clock = ClockDisplay([24, 60])
-#         clock.numbers[1].value = 61
-#         clock.numbers[1].limit = 60
-#         assert clock.numbers[1].invariant() is False
-
-#     def test13(self):   # display_number 9: mutation from <class 'ast.Eq'> to <class 'ast.Lt'>
-#         clock = ClockDisplay([24, 60])
-#         clock.numbers[1].value = -1
-#         assert clock.numbers[1].increase() is True
-
-#     def test14(self):   # display_number 9: mutation from <class 'ast.Eq'> to <class 'ast.LtE'>
-#         clock = ClockDisplay([24, 60])
-#         clock.numbers[1].limit = -60
-#         assert clock.numbers[1].increase() is False
-
-#     def test15(self):   # display_number 16: mutation from <class 'ast.Lt'> to <class 'ast.LtE'>
-#         clock = ClockDisplay([24, 60])
-#         clock.numbers[1].value = 10
-#         assert clock.numbers[1].str() == "10"
-
-#     def test16(self):   # display_number 8: mutation from <class 'ast.Mod'> to <class 'ast.Pow'>
-#         # display_number 8: mutation from <class 'ast.Add'> to <class 'ast.Mod'
-#         # display_number 8: mutation from <class 'ast.Add'> to <class 'ast.Sub'>
-#         # Se supone que no hay valores x e y que cumplan
-#         # (x + y + 1) % y != (x % y + 1) % y
-#         # (x + y + 1) % y != (x - y + 1) % y
-#         number = NumberDisplay(3, 5)
-#         assert number.increase() is False
-#         assert number.value == 4
+    def test7(self):
+        clock_display = ClockDisplay([24,60,60])
+        clock_display.numbers = [NumberDisplay(23,24), 
+                                NumberDisplay(59,60),
+                                NumberDisplay(59,60)]
+        result = clock_display.invariant()
+        self.assertEqual(result, True)
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+    ##obs: list(map(fx, iterable))
+        ## fx se aplica sobre cada elemento del iterable  (map)
+        ## y se retorna una lista
+
+    ## all(iterable) -> retorna True solo si todos los elementos del
+    #  iterable tienen un valor booleano de True o el iterable 
+    #  está vacío, en los demás casos retornara False
+
+    ## %: operación resto
+
+
+    ### MUTA TESTS
+
+    def test8(self):
+        clock = ClockDisplay([24, 60, 60])
+        for i in range(4):
+            clock.increment()
+        assert clock.str() == "00:00:04"
+
+    def test9(self): # NO FUNCIONANDO
+        print("TEST 8")
+        clock = ClockDisplay([24, 60])
+        print(clock.str())
+        print(clock.numbers[0].str())
+        print(clock.numbers[1].str())
+        clock.numbers.pop()
+        print(clock.str())
+        print("largo", len(clock.numbers) - 1)
+        clock.increment()
+
+    def test10(self):
+        clock = ClockDisplay([24, 60])
+        clock.numbers[1].increase()
+        assert clock.numbers[1].str() == "01"
+
+    def test11(self):
+        clock = ClockDisplay([24, 60])
+        clock.numbers[1].value = 100
+        assert clock.numbers[1].str() == "100"
+
+    def test12(self):   # display_number 21: mutation from <class 'ast.Lt'> to <class 'ast.LtE'>
+        clock = ClockDisplay([24, 60])
+        clock.numbers[1].value = 60
+        clock.numbers[1].limit = 60
+        assert clock.numbers[1].invariant() is False
+
+    def test13(self):    # display_number 21: mutation from <class 'ast.Lt'> to <class 'ast.Gt'>
+        clock = ClockDisplay([24, 60])
+        clock.numbers[1].value = 61
+        clock.numbers[1].limit = 60
+        assert clock.numbers[1].invariant() is False
+
+    def test14(self):   # display_number 9: mutation from <class 'ast.Eq'> to <class 'ast.Lt'>
+        clock = ClockDisplay([24, 60])
+        clock.numbers[1].value = -1
+        assert clock.numbers[1].increase() is True
+
+    def test15(self):   # display_number 9: mutation from <class 'ast.Eq'> to <class 'ast.LtE'>
+        clock = ClockDisplay([24, 60])
+        clock.numbers[1].limit = -60
+        assert clock.numbers[1].increase() is False
+
+    def test16(self):   # display_number 16: mutation from <class 'ast.Lt'> to <class 'ast.LtE'>
+        clock = ClockDisplay([24, 60])
+        clock.numbers[1].value = 10
+        assert clock.numbers[1].str() == "10"
+
+    def test17(self):   # display_number 8: mutation from <class 'ast.Mod'> to <class 'ast.Pow'>
+        # display_number 8: mutation from <class 'ast.Add'> to <class 'ast.Mod'
+        # display_number 8: mutation from <class 'ast.Add'> to <class 'ast.Sub'>
+        # Se supone que no hay valores x e y que cumplan
+        # (x + y + 1) % y != (x % y + 1) % y
+        # (x + y + 1) % y != (x - y + 1) % y
+        number = NumberDisplay(3, 5)
+        assert number.increase() is False
+        assert number.value == 4
+
+
+if __name__ == '__main__':
+    unittest.main()
